bcb133943fe756544e128452d00ba6bc52937394
Zaki Zuberi <zzuberi@google.com>
2025-07-30 16:12:13 -0700

    Updating setting text to specify minimum token lenght

--- ui/src/plugins/com.google.PerfettoMcp/index.ts
+++ ui/src/plugins/com.google.PerfettoMcp/index.ts
@@ -45,7 +45,7 @@ export default class PerfettoMcpPlugin implements PerfettoPlugin {
     PerfettoMcpPlugin.promptSetting = app.settings.register({
       id: `${app.pluginId}#PromptSetting`,
       name: 'Gemini Prompt',
-      description: 'Upload a .txt file containing the initial Gemini prompt.',
+      description: 'Upload a .txt file containing the initial Gemini prompt. (minimum of 2048 tokens)',
       schema: z.string(),
       defaultValue: "",
       render: (setting) => {
